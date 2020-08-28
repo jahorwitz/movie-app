1ebc9858df74b09aac69d6b948c035ec162b60bf
Jonathan Alumbaugh <jalumbaugh@duffyshanley.com>
2020-08-27 20:43:20 -0400

    Format on save

--- src/components/movie/index.js
+++ src/components/movie/index.js
@@ -1,36 +1,39 @@
-import React, {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import Axios from 'axios'
 
-
 function Movie() {
-  let {movieID} = useParams()
+  let { movieID } = useParams()
   let [movieDetail, setMovieDetails] = useState(null)
-  
-  useEffect (() => {
+
+  useEffect(() => {
     var config = {
       method: 'get',
       url: `https://api.themoviedb.org/3/movie/${movieID}?api_key=d395b794def74b8f16ec114b3268cee8`,
-      headers: { }
-    };
+      headers: {},
+    }
     Axios(config)
-    .then(function (response) {
-      setMovieDetails(response.data)
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }, [])
-  console.log(movieDetail);
+      .then(function (response) {
+        setMovieDetails(response.data)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }, [movieID])
+
+  console.log(movieDetail)
   return (
     <div className="Movie">
       <header className="Movie-header">
-        <h1>{movieDetail?.original_title}</h1>
-        <h2>Movie tagline</h2>
+        <div>
+          <img src="#" alt="Movie cover" />
+        </div>
+        <div>
+          <h1>{movieDetail?.original_title}</h1>
+          <h2>Movie tagline</h2>
 
-        <img src="#" alt="Movie cover" />
-
-        <b>Movie rating</b>
+          <b>Movie rating</b>
+        </div>
       </header>
 
       <div>
